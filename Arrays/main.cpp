46eda97783aff2b84527d53c3deaa035d3aab9bc
zheka <zheka@HOME-PC>
2024-03-17 20:31:42 +0300

    Add project files.

---
+++ Arrays/main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+#define tab "\t"
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int SIZE = 5;
+	int arr[SIZE];
+
+	cout << "Введите элементы массива: ";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cin >> arr[i];
+	}
+
+		
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	for (int i = SIZE - 1; i >= 0; i--)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	int sum = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "Сумма элементов массива: " << sum << endl;
+	cout << "Среднее-арифметическое: " << (double)sum / SIZE << endl;
+
+	int min, max;
+	min = max = arr[0];
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+		if (arr[i] > max)max = arr[i];
+	}
+	cout << "Минимальное значение в массиве: " << min << endl;
+	cout << "Максимальное значение в массиве: " << max << endl;
+}
---
+++ Shift/main.cpp
@@ -0,0 +1,31 @@
+//SHIFT
+#include<iostream>
+using namespace std;
+#define tab "\t"
+void main()
+{
+	setlocale(LC_ALL, "");
+	int r;
+	cout << "введите кол-во сдвигов: "; cin >> r;
+	const int n = 10;
+	int arr[n] = { 0, 1 ,1, 2, 3, 5, 8, 13, 21, 34 };
+	
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	for (int i = 0+r; i < n; i++)
+	{
+		cout << arr[i]<< tab;
+		
+	}
+	cout << endl;
+	
+	cout << "¬ведите значение элементов: ";
+	for (int)
+		
+	
+}
