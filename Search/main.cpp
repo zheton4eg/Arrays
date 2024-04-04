62002795d0d1320c93fc6c02fb4ccb8122ff07bd
zheka <zheka@HOME-PC>
2024-04-04 17:59:53 +0300

    upgrade2

--- Search/main.cpp
+++ Search/main.cpp
@@ -8,23 +8,22 @@ void main()
 	int array[n] = {};
 	int count[m] = {0};
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) 
+	{
 		array[i] = rand() % 7 - 0;
 		cout << array[i] << tab;
 	}
-	
 	// Подсчитываем количество каждого элемента в массиве:
-	for (int i = 0; i < n; i++) {	
+	for (int i = 0; i < n; i++) 
+	{	
 	count[array[i]]++;
 	}
 	cout << endl;
-	bool duplicatesFound = false;
 	// Выводим только повторяющиеся элементы:
 	for (int i = 0; i < m; i++)
 	{
 		if (count[i] > 1)
-		{
-			duplicatesFound = true;
+		{	
 			cout << "Число: " << i << ", Количество в массиве: " << count[i] << endl;
 		}
 	}
